5a0d10ea6948196ac34c2e7e068a93a4ccaa9ff0
RyderOfTheVeil <46351330+RyderOfTheVeil@users.noreply.github.com>
2019-07-18 14:46:10 -0500

    Create GetLoggedOnUsers.ps1
    Gets the logged on users. Like many other items in my trove, this requires administrative access to the servers in question.

---
+++ GetLoggedOnUsers.ps1
@@ -0,0 +1,18 @@
+#This requires Administrative access of systems queried
+function Get-LoggedOnUser 
+ {
+     [CmdletBinding()]
+     param
+     (
+         [Parameter()]
+         [ValidateScript({ Test-Connection -ComputerName $_ -Quiet -Count 1 })]
+         [ValidateNotNullOrEmpty()]
+         [string[]]$ComputerName = $env:COMPUTERNAME
+     )
+     foreach ($comp in $ComputerName)
+     {
+         $output = @{ 'ComputerName' = $comp }
+         $output.UserName = (Get-WmiObject -Class win32_computersystem -ComputerName $comp).UserName
+         [PSCustomObject]$output
+     }
+ }#END FUNCTION Get-LoggedOnUser

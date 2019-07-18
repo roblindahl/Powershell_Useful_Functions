ddcbac7cdd6c70dad3fcf34a405d99f99bf9c8f6
RyderOfTheVeil <46351330+RyderOfTheVeil@users.noreply.github.com>
2019-07-18 15:24:14 -0500

    Update GetSSASInfo.ps1

--- GetSSASInfo.ps1
+++ GetSSASInfo.ps1
@@ -1,17 +1,17 @@
 FUNCTION GETSSASINFO{
     [cmdletbinding()]
     PARAM(
-    [string]$connection
-    )
+          [string]$connection = "localhost"
+         )
 ## Connect and get the edition of the local server
-$connection = "localhost"
-$server = New-Object Microsoft.AnalysisServices.Server
-$server.connect($connection)
-    Write-Output ("`n`nServer: {0}`nEdition: {1}`nBuild: {2}`n`n" -f 
-    $server.Name, $server.Edition, $server.Version)
-foreach ($db in $server.Databases){
-    Write-Output ("DatabaseName: {0} Size: {1} Version: {2}" -f 
-    $db.Name, $db.EstimatedSize, $db.Version)
-}
+Add-Type -AssemblyName 'Microsoft.AnalysisServices, Version=14.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91'
 
+    $server = New-Object Microsoft.AnalysisServices.Server
+    $server.connect($connection)
+        Write-Output ("`n`nServer: {0}`nEdition: {1}`nBuild: {2}`n`n" -f 
+        $server.Name, $server.Edition, $server.Version)
+    foreach ($db in $server.Databases){
+        Write-Output ("DatabaseName: {0} Size: {1} Version: {2}" -f 
+        $db.Name, $db.EstimatedSize, $db.Version)
+    }
 }#END FUNCTION GETSSASINFO
